025b4c3603427f177a2bfe5c86d91b5511de1a19
mateo <m.devia@uniandes.edu.co>
2019-12-07 08:10:53 -0500

    front build

---
+++ frontend/build/precache-manifest.0af909ce3f068cb5b582c3cf428d45dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1d5044fa9831ef7457dba9f11d82966",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5ea1a95dd94b17ca990",
+    "url": "/static/css/main.3f13ea9c.chunk.css"
+  },
+  {
+    "revision": "a7306d7f67c36514b6f6",
+    "url": "/static/js/2.2a203a25.chunk.js"
+  },
+  {
+    "revision": "e5ea1a95dd94b17ca990",
+    "url": "/static/js/main.286dc244.chunk.js"
+  },
+  {
+    "revision": "4c40b94f0748754179e8",
+    "url": "/static/js/runtime-main.bbc06ef5.js"
+  },
+  {
+    "revision": "4ba539d0da866b0ee27f444b0c0cae64",
+    "url": "/static/media/taxi.4ba539d0.svg"
+  }
+]);
